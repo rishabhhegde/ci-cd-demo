e358e563494335d7bb49bf3b11e253ae01a9b896
rishabhhegde <rishabhhegde8@gmail.com>
2025-09-02 17:41:33 +0530

    deleting all repo files

--- tests/test_app.py
+++
@@ -1,8 +0,0 @@
-from app import app
-
-def test_home():
-    with app.test_client() as c:
-        resp = c.get("/")
-        assert resp.status_code == 200
-        data = resp.get_json()
-        assert data["message"] == "Hello CI/CD!"
